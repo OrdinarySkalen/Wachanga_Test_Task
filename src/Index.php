2ebc5e3d39c25b44134434badec9232fb2e1d53a
OrdynarySkalen <voron-37@mail.ru>
2017-05-14 16:26:37 +0700

    Добавим кнопок

--- src/Index.php
+++ src/Index.php
@@ -4,18 +4,29 @@
 <meta charset="utf-8">
 </head>
 <body>
+<form action="app.php" method="POST">
+    Balls in user's basket: <input type="number" name="userBasketCount" value=100 /><br><br>
+    Count of baskets: <input type="number" name="basketCount" value=30 /><br><br>
+    <input type="submit" value="Try again">
+</form>
 <div>
 <?php
 include "Basket.php";
 include "Comparator.php";
 
+$userBasketCount = 100;
+$basketCount = 31;
+
+if(isset($_POST['userBasketCount'])) $userBasketCount = $_POST['userBasketCount'];
+if (isset($_POST['basketCount'])) $basketCount = $_POST['basketCount'];
+
 $taskB="";
 $taskC="";
 $userBasket = new Basket();
-$userBasket->fillBasket(100);
+$userBasket->fillBasket($userBasketCount);
 $userBasket->printBasket("User basket");
 print "<br>";
-for ($i=1; $i<31;$i++)
+for ($i=1; $i<$basketCount+1;$i++)
 {
 	$basket = new Basket();
 	$basket->fillBasket(rand(1, 9));
@@ -33,6 +44,5 @@ print "<br><br>Contained in User's basket: $taskB";
 print "<br>Have only one element from User basket: $taskC";
 ?>
 </div>
-</form>
 </body>
 </html>
